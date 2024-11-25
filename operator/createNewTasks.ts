e4e34c22586be16280f236487c3381fd31c90cd9
Ammar Robbani <amr.robb@gmail.com>
2024-11-25 19:08:39 +0700

    update createNewTask file

--- operator/createNewTasks.ts
+++ operator/createNewTasks.ts
@@ -38,11 +38,11 @@ const crediflexServiceManager = new ethers.Contract(
 function generateRandomData(): string {
 	const walletAddresses = [
 		"0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D", // Ethereum Foundation Donation Address
-		// "0xD8dA6BF26964aF9D7eEd9e03E53415D37aA96045", // Vitalik Buterin's Public Wallet
-		// "0x0000000000000000000000000000000000000000", // Testing or Faucet Address
-		// "0x022bAD9eb609D7F875051217C1C4c6404250C5e7", // Example Address
-		// "0x8757F328371E571308C1271BD82B91882253FDd1", // Example Address
-		// "0x77C037fbF42e85dB1487B390b08f58C00f438812",
+		"0xD8dA6BF26964aF9D7eEd9e03E53415D37aA96045", // Vitalik Buterin's Public Wallet
+		"0x0000000000000000000000000000000000000000", // Testing or Faucet Address
+		"0x022bAD9eb609D7F875051217C1C4c6404250C5e7", // Example Address
+		"0x8757F328371E571308C1271BD82B91882253FDd1", // Example Address
+		"0x77C037fbF42e85dB1487B390b08f58C00f438812",
 	];
 	const randomWalletAddress =
 		walletAddresses[Math.floor(Math.random() * walletAddresses.length)];
@@ -72,7 +72,7 @@ function startCreatingTasks() {
 		`Creating new task to calculte CScore for address: ${randomData}`
 	);
 	createNewTask(randomData);
-	// checkCScore("0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D");
+	// checkCScore(randomData);
 
 	// Uncomment this to create tasks every 30 seconds
 	// setInterval(() => {
@@ -86,7 +86,7 @@ function startCreatingTasks() {
 
 async function checkCScore(user: string) {
 	try {
-		const result = await crediflexServiceManager.cScores(user);
+		const result = await crediflexServiceManager.getUserCScoreData(user);
 		console.log("CScore for user:", user, "is", result);
 	} catch (error) {
 		console.error("Error fetching CScore for user:", user, error);
